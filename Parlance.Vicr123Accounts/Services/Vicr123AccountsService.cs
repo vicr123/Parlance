71a8ad9d1216220336bd9cba1f6983fb082753ab
Victor Tran <vicr12345@gmail.com>
2022-08-12 00:08:05 +1000

    Implement some DBus services

--- Parlance.Vicr123Accounts/Services/Vicr123AccountsService.cs
+++ Parlance.Vicr123Accounts/Services/Vicr123AccountsService.cs
@@ -1,6 +1,22 @@
+using accounts.DBus;
+using Tmds.DBus;
+
 namespace Parlance.Vicr123Accounts.Services;
 
 public class Vicr123AccountsService : IVicr123AccountsService
 {
-    
+    private Connection _connection;
+    private IManager _manager;
+    public Vicr123AccountsService()
+    {
+        InitAsync().Wait();
+    }
+
+    public async Task InitAsync()
+    {
+        _connection = new Connection("unix:path=/var/vicr123-accounts/vicr123-accounts-bus");
+        await _connection.ConnectAsync();
+
+        _manager = _connection.CreateProxy<IManager>("com.vicr123.accounts", "/com/vicr123/accounts");
+    }
 }
