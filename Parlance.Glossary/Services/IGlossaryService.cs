97c32588223635f37e2f2ab7d6f455243811ddb9
Victor Tran <vicr12345@gmail.com>
2023-08-01 18:43:02 +1000

    Implement adding terms

--- Parlance.Glossary/Services/IGlossaryService.cs
+++ Parlance.Glossary/Services/IGlossaryService.cs
@@ -1,3 +1,4 @@
+using Parlance.CldrData;
 using Parlance.Database.Models;
 
 namespace Parlance.Glossary.Services;
@@ -10,4 +11,8 @@ public interface IGlossaryService
     Task ConnectGlossary(Database.Models.Glossary glossary, Project project);
     Database.Models.Glossary GlossaryById(Guid id);
     Task DisconnectGlossary(Database.Models.Glossary glossary, Project project);
+    GlossaryItem GlossaryItemById(Guid id);
+    Task Define(Database.Models.Glossary glossary, string term, string translation, Locale locale);
+    Task RemoveDefinition(GlossaryItem item);
+    IEnumerable<GlossaryItem> GetTerms(Database.Models.Glossary glossary, Locale locale);
 }
