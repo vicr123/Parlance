d91a6c383bac8e35f3daa45a4c0a8d036c776ebf
Victor Tran <vicr12345@gmail.com>
2024-01-26 22:30:28 +1100

    Make sure to run genlangindex

--- Parlance.ClientApp/genlangindex.js
+++ Parlance.ClientApp/genlangindex.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
-const fs = require('fs/promises');
+import fs from "fs/promises";
 
-(async () => {
-    let availableTranslations = await fs.readdir("public/resources/translations");
-    availableTranslations = availableTranslations.filter(x => x !== "index.json");
-    await fs.writeFile("public/resources/translations/index.json", JSON.stringify(availableTranslations));
-})();
+let availableTranslations = await fs.readdir("public/resources/translations");
+availableTranslations = availableTranslations.filter(x => x !== "index.json");
+await fs.writeFile("public/resources/translations/index.json", JSON.stringify(availableTranslations));
