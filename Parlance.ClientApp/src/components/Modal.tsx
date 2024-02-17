d71cc1b83ee4fadd6285ab9acafb8aca7295cdc6
Victor Tran <vicr12345@gmail.com>
2024-02-17 22:30:22 +1100

    Clean up modal code

--- Parlance.ClientApp/src/components/Modal.tsx
+++ Parlance.ClientApp/src/components/Modal.tsx
@@ -14,7 +14,7 @@ interface ModalButton {
     text: string
 }
 
-interface ModalExportProps {
+interface ModalProps {
     popover?: any
     buttons?: ModalButton[]
     onButtonClick?: () => {}
@@ -24,21 +24,12 @@ interface ModalExportProps {
     topComponent?: ReactNode
 }
 
-interface ModalProps extends WithTranslation, ModalExportProps {
-}
-
-interface ModalState {
-    
-}
-
-type ModalComponentExportType = new(ModalProps: any) => React.Component<ModalExportProps, ModalState>;
-
 interface ModalExportButtons {
     CancelButton: ModalButton;
     OkButton: ModalButton;
 }
 
-interface ModalExports extends ModalComponentExportType, ModalExportButtons {
+interface ModalExports extends React.FunctionComponent<ModalProps>, ModalExportButtons {
     mount: (modal: React.ReactElement) => void;
     unmount: () => void;
     ModalProgressSpinner: typeof ModalProgressSpinner
@@ -48,71 +39,46 @@ interface ModalProgressSpinnerProps {
     message?: string;
 }
 
-class Modal extends React.Component<ModalProps, ModalState> {
-    render() {
-        return <div
-            className={`${Styles.ModalBackground} ${i18n.dir()} ${this.props.popover && Styles.PopoverBackground}`}
-            dir={i18n.dir()}>
-            <div className={`${Styles.ModalContainer} ${this.props.popover && Styles.PopoverContainer}`}>
-                {this.renderHeading()}
-                {this.renderTopComponent()}
-                {this.renderModalText()}
-                {this.renderModalList()}
-                <div className={Styles.ModalButtonContainer}>
-                    {this.props.buttons?.map(button => {
-                        if (typeof button === "object") {
-                            let classes = [Styles.ModalButton];
-                            if (button.destructive) classes.push(Styles.DestructiveModalButton);
-                            return <div onClick={button.onClick} className={classes.join(" ")}
-                                        key={button.text}>{button.text}</div>
-                        } else {
-                            // deprecated
-                            // @ts-ignore
-                            return <div onClick={this.props.onButtonClick?.bind(this, button)}
-                                        className={Styles.ModalButton} key={button}>{button}</div>
-                        }
-                    })}
-                </div>
+function Modal({heading, topComponent, popover, onBackClicked, buttons, children}: ModalProps) {
+    // @ts-ignore
+    let modalText = React.Children.toArray(children).filter(child => child.type?.displayName !== "ModalList");
+    // @ts-ignore
+    let modalList = React.Children.toArray(children).filter(child => child.type?.displayName === "ModalList");
+
+    return <div
+        className={`${Styles.ModalBackground} ${i18n.dir()} ${popover && Styles.PopoverBackground}`}
+        dir={i18n.dir()}>
+        <div className={`${Styles.ModalContainer} ${popover && Styles.PopoverContainer}`}>
+            {heading && <div className={Styles.ModalHeading}>
+                {popover &&
+                    <div className={Styles.BackButton} onClick={onBackClicked}><Icon icon={"go-previous"}
+                                                                                     flip={true}/></div>}
+                <span className={Styles.HeadingText}>{heading}</span>
+            </div>}
+            {topComponent && <div className={Styles.ModalComponent}>
+                {topComponent}
+            </div>}
+            {modalText && <div className={Styles.ModalText}>
+                {modalText}
+            </div>}
+            {modalList}
+            <div className={Styles.ModalButtonContainer}>
+                {buttons?.map(button => {
+                    if (typeof button === "object") {
+                        let classes = [Styles.ModalButton];
+                        if (button.destructive) classes.push(Styles.DestructiveModalButton);
+                        return <div onClick={button.onClick} className={classes.join(" ")}
+                                    key={button.text}>{button.text}</div>
+                    } else {
+                        // deprecated
+                        // @ts-ignore
+                        return <div onClick={props.onButtonClick?.bind(this, button)}
+                                    className={Styles.ModalButton} key={button}>{button}</div>
+                    }
+                })}
             </div>
         </div>
-    }
-
-    private renderModalList() {
-        // @ts-ignore
-        let children = React.Children.toArray(this.props.children).filter(child => child.type?.displayName === "ModalList")
-
-        return children.length !== 0 && <>{children}</>
-    }
-
-    private renderModalText() {
-        // @ts-ignore
-        let children = React.Children.toArray(this.props.children).filter(child => child.type?.displayName !== "ModalList")
-
-        return children.length !== 0 && <div className={Styles.ModalText}>
-            {children}
-        </div>
-    }
-
-    private renderHeading() {
-        if (this.props.heading) {
-            return <div className={Styles.ModalHeading}>
-                {this.props.popover &&
-                    <div className={Styles.BackButton} onClick={this.props.onBackClicked}><Icon icon={"go-previous"}
-                                                                                                flip={true}/></div>}
-                <span className={Styles.HeadingText}>{this.props.heading}</span>
-            </div>
-        }
-        return null;
-    }
-
-    private renderTopComponent() {
-        if (this.props.topComponent) {
-            return <div className={Styles.ModalComponent}>
-                {this.props.topComponent}
-            </div>;
-        }
-        return null;
-    }
+    </div>
 }
 
 function ModalProgressSpinner(props : ModalProgressSpinnerProps) {
@@ -136,7 +102,7 @@ i18n.on("initialized", setStandardButtons);
 i18n.on("languageChanged", setStandardButtons);
 i18n.on("loaded", setStandardButtons);
 
-let ExportProperty = withTranslation()(Modal) as ModalExports;
+let ExportProperty = Modal as ModalExports;
 ExportProperty.mount = (modal) => {
     if (root) ExportProperty.unmount();
     root = createRoot(document.getElementById('modalContainer')!);
